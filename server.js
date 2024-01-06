4d7ecf5a8dea1979cf57abde159c91784662fa57
Wanessa Lopes <wanessaalopes@gmail.com>
2024-01-05 19:48:53 -0700

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const port = 8080;
 const app = express();
+app.use(express.static("public"));
+app.use((req, res, next) => {
+  console.log(
+    `Time: ${new Date().toLocaleString()} - Request Path: ${req.path}`
+  );
+  next();
+});
+const foodRoutes = require("./routes/foodRoutes");
+
+app.use('/foods', foodRoutes);
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -9,3 +20,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
+
